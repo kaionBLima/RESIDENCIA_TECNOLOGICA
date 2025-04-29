a84fd138f2e25149f812e7f0b66575d19d66f0ba
kaionBLima <kaionbrandaolima@gmail.com>
2025-04-29 12:12:17 -0300

    Upload file

--- Aulas/directors_movies.py
+++ Aulas/directors_movies.py
@@ -1,13 +1,29 @@
-import pandas as pd
-
+import pandas as pd 
+ 
+# Ex.4
 df = pd.read_csv('movies.csv')
 
-def receita(revenue):
-    if revenue > 1000000:
-        return 'Sim'
-    else:
-        return 'Não'
+# def receita(revenue):
+#     if revenue > 1000000:
+#         return 'Sim'
+#     else:
+#         return 'Não'
     
-df['sucesso'] = df['revenue'].apply(receita)
+# df['sucesso'] = df['revenue'].apply(receita)
 
-print(df[['title', 'revenue','sucesso']])
+# print(df[['title', 'revenue','sucesso']])
+
+# Ex.5
+
+df_media = df.groupby('year')['vote_average'].mean().reset_index(name='Nota_Media')
+print(df_media.head())
+
+df_MaiorMed = df_media.max()
+print("\n\n", df_MaiorMed.head())
+
+df_imp = df.groupby('year').agg(
+    NumFilmes = ('title', 'count'),
+    nota_media=('vote_average', 'mean')
+)
+
+print("\n\n", df_imp.head())

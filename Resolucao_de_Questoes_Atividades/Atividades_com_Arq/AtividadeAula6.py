b8fc87e4f7fab3c1fd411f64625e9961f4faf6c4
kaionBLima <kaionbrandaolima@gmail.com>
2025-05-05 23:45:39 -0300

    Upload file activity

--- Resolucao_de_Questoes_Atividades/Atividades_com_Arq/AtividadeAula6.py
+++ Resolucao_de_Questoes_Atividades/Atividades_com_Arq/AtividadeAula6.py
@@ -37,3 +37,41 @@ dfMediaSal = df.groupby('departamento')['salario'].mean()
 # Questao 10:
 # filter = df[df['nome'].str.startswith('A')]
 # print(filter)
+
+# Questao 11:
+# print(df['nome'].duplicated())
+
+# Questao 12:
+df2 = pd.read_csv('dados2.csv')
+# df_concat = pd.concat([df, df2], ignore_index=False)
+# print(df_concat)
+
+# Questao 13:
+df_merger = pd.merge(
+    df, 
+    df2, 
+    on=['id'],
+    how='inner'
+)
+# print(df_merger)
+
+# Questao 14:
+dfcont = df['estado'].unique()
+# print(dfcont)
+
+# Questao 15:
+def faixa_etaria(idade):
+    if idade <= 18:
+        return 'jovem'
+    elif idade < 45:
+        return 'adulto'
+    else: 
+        return 'idoso'
+    
+df['Faixa-etaria'] = df['idade'].apply(faixa_etaria)
+print(df)
+
+# Questao 16:
+df = df.drop_duplicates(subset=['email', 'telefone'])
+print(df)
+
